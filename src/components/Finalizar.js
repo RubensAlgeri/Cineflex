0a4217adfbbb2f3c21566944736f34a09a30d588
Rubens Algeri <rubens_zys@hotmail.com>
2022-03-28 11:25:32 -0300

    alterar nome de variavel e adicionar alerta de erro

--- src/components/Finalizar.js
+++ src/components/Finalizar.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from "react-router-dom";
-import {cpfMask} from "./Mask"
+import {cpfMask} from "./MascaraCPF"
 import Rodape from './Rodape';
 
 export default function Finalizar(){
